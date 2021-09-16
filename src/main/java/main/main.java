eca6a826f477f2b55b9afaf726d345eeb38088d7
singhAvneet <asingh@gms.ca>
2021-09-15 22:50:53 -0600

    JSON entries validation

--- src/main/java/main/main.java
+++ src/main/java/main/main.java
@@ -4,26 +4,43 @@ import com.google.gson.Gson;
 import modal.Log;
 
 import java.io.*;
+import java.lang.reflect.Type;
+import java.util.*;
 
 public class main {
 
 
-    public static void main( String[] args ) throws FileNotFoundException {
-
-        File file = new File("log.dat");
+    public static void main( String[] args ) throws IOException {
+        String line;
+        Log logEntry = new Log();
         Gson gson = new Gson();
-        try (BufferedReader br = new BufferedReader(new FileReader(file)))
-        {
-            String line;
+        Map<String, Integer> uniqueList=new HashMap<String, Integer>();
+       BufferedReader br = new BufferedReader(new FileReader(new File("log.dat")));
+
+
             while ((line = br.readLine()) != null) {
-                Log log = gson.fromJson(line, Log.class);
-                System.out.println(log.getPh());
+                try{
+                    logEntry = gson.fromJson(line, Log.class);
+
+                    if(logEntry.isValid()){
+                        System.out.println(logEntry.isValid());
+                    }else{
+                        System.out.println("JSON entries in line "+line+" is invalid.");break;
+                    }
+                }
+                catch(Exception e){
+                    System.out.println("Invalid Log File");
+                    System.out.println("Error in "+line+". Error: "+ e.getMessage());break;
+                }
+
+
+
             }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+
 
 
 
     }
+
+
 }
--- src/main/java/modal/Log.java
+++ src/main/java/modal/Log.java
@@ -1,6 +1,17 @@
 package modal;
 
+
+import jdk.jfr.Timestamp;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.util.regex.Pattern;
+
 public class Log {
+
+
     private long ts;//:  timestamp
     private int pt;//:  processing time
     private String si;//:  session ID
@@ -9,78 +20,63 @@ public class Log {
     private String sha;//: sha256 of the file
     private String nm;//:  file name
     private String ph;//:  path
-    private int dp;//:  disposition (valid values: MALICIOUS (1), CLEAN (2), UNKNOWN (3))
+    private byte dp;//:  disposition (valid values: MALICIOUS (1), CLEAN (2), UNKNOWN (3))
 
+    private boolean isValid=false,isNotEmpty=false;
+
+    public boolean isValid(){
+         isNotEmpty= getPh()!=null || getBg()!=null || getDp()!=0 ||
+                getNm()!=null || getPt()!=0 || getTs()!=0 ||
+                getUu()!=null || getSha()!=null || getSi()!=null;
+
+        isValid=String.valueOf(getTs()).matches("[0-9]{10}")
+                &&  String.valueOf(getPt()).matches("^\\d+$")
+                &&  getSi().matches("^#?([a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})$")
+                &&  getUu().matches("^#?([a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})$")
+                &&  getBg().matches("^#?([a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})$")
+                &&  getSha().matches("^#?([a-f0-9]{64})$")
+                &&  getNm().matches(".*")
+                &&  getPh().matches("^[^/]*/[^/].*$")
+                &&  String.valueOf(getDp()).matches("[1-3]{1}")
+                && getDp()==2;
+
+
+        return isValid && isNotEmpty;
+    }
 
     public long getTs() {
         return ts;
-    }
-
-    public void setTs(long ts) {
-        this.ts = ts;
     }
 
     public int getPt() {
         return pt;
     }
 
-    public void setPt(int pt) {
-        this.pt = pt;
-    }
-
     public String getSi() {
         return si;
-    }
-
-    public void setSi(String si) {
-        this.si = si;
     }
 
     public String getUu() {
         return uu;
     }
 
-    public void setUu(String uu) {
-        this.uu = uu;
-    }
-
     public String getBg() {
         return bg;
-    }
-
-    public void setBg(String bg) {
-        this.bg = bg;
     }
 
     public String getSha() {
         return sha;
     }
 
-    public void setSha(String sha) {
-        this.sha = sha;
-    }
-
     public String getNm() {
         return nm;
-    }
-
-    public void setNm(String nm) {
-        this.nm = nm;
     }
 
     public String getPh() {
         return ph;
     }
 
-    public void setPh(String ph) {
-        this.ph = ph;
-    }
-
-    public int getDp() {
+    public byte getDp() {
         return dp;
-    }
-
-    public void setDp(int dp) {
-        this.dp = dp;
     }
 }
